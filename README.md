5e305379cf5c3042f03d923dbca0743c749898b9
Stéphane B <stillfury@hotmail.com>
2020-10-21 21:01:28 -0400

    add README

---
+++ README.md
@@ -0,0 +1,6 @@
+See [Caniuse](https://caniuse.com/midi) for browser support.  No Safari nor FireFox up until now.
+
+## TODO
+Add Jean-Philippe Côté's [WebMidi.js](https://github.com/djipco/webmidi) library which would simplify two things:
+  * sending outgoing MIDI messages
+  * reacting to incoming MIDI messages.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("hello MIDI");
+if (navigator.requestMIDIAccess) {
+  console.log("This browser supports WebMIDI!");
+} else {
+  console.log("WebMIDI is not supported in this browser.");
+}
+
+navigator.requestMIDIAccess().then(onMidiSuccess, onMidifailure);
+
+function onMidiSuccess(midiAccess) {
+  for (const input of midiAccess.inputs.values()) {
+    input.onmidimessage = getMidiMessage;
+  }
+}
+
+function onMidifailure() {
+  console.log("Could not access your MIDI devices.");
+}
+
+function getMidiMessage(message) {
+  const command = message.data[0];
+  const note = message.data[1];
+  const velocity = message.data.length > 2 ? message.data[2] : 0; // a velocity value might not be included with a noteOff command
+
+  switch (command) {
+    case 144: // noteOn
+      if (velocity > 0) {
+        noteOn(note, velocity);
+      } else {
+        noteOff(note);
+      }
+      break;
+    case 128: // noteOff
+      noteOff(note);
+      break;
+    // we could easily expand this switch statement to cover other types of commands such as controllers or sysex
+  }
+}
