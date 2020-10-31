0f8e1ccaab6263d1df0b861e7faee87c10e8f640
Stéphane B <stillfury@hotmail.com>
2020-10-31 14:42:31 -0400

    remove mp3 from app and fetch them from s3 instead

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,68 @@
-import WebMidi, { InputEventNoteoff, InputEventNoteon } from "webmidi";
+import WebMidi, {
+  Input as MidiPortInput,
+  InputEventNoteoff,
+  InputEventNoteon,
+} from "webmidi";
 
 enum Keys {
   C4 = "C4",
   D4 = "D4",
   E4 = "E4",
 }
+const BUCKET_URL =
+  "https://boomtap-proto-test-sample-bucket.s3.ca-central-1.amazonaws.com/";
 
-const soundPaths = Object.values(require("./sounds/*.mp3"));
-const sounds = soundPaths.map((path: string) => new Audio(path));
+const SAMPLE_NAMES = ["mouth_snare.mp3", "mouth_kick.mp3", "mouth_hihat.mp3"];
+
+async function fetchSamples() {
+  let sampleBuffers = [];
+  await Promise.all(
+    SAMPLE_NAMES.map(async (name) => {
+      const response = await fetch(BUCKET_URL + name);
+      const arrayBuffer = await response.arrayBuffer();
+      const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+      sampleBuffers.push(audioBuffer);
+    })
+  );
+  return sampleBuffers;
+}
+
+const audioCtx = new AudioContext();
+
+function play(audioBuffer: AudioBuffer) {
+  const source = audioCtx.createBufferSource();
+  source.buffer = audioBuffer;
+  source.connect(audioCtx.destination);
+  source.start();
+}
+
+let keyMap;
+
 const keys = [Keys.C4, Keys.D4, Keys.E4];
-const keyMap = Object.fromEntries(sounds.map((sound, i) => [keys[i], sound]));
 
-const lightUp = (square: HTMLElement): void => {
-  square.style.backgroundColor = "blue";
+const createKeyMap = async () => {
+  const sampleBuffers = await fetchSamples();
+  keyMap = Object.fromEntries(
+    sampleBuffers.map((sound, i) => [keys[i], sound])
+  );
 };
 
-const lightOff = (square: HTMLElement): void => {
-  square.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+createKeyMap();
+
+const toggleColor = (square: HTMLElement): void => {
+  square.classList.toggle("lit");
 };
 
 const playback = (note: Keys): void => {
-  keyMap[note].play();
+  play(keyMap[note]);
+};
+
+type MidiEventTypes = "noteon" | "noteoff";
+
+const listenTo = (eventTypes: MidiEventTypes[]) => (input: MidiPortInput) => {
+  eventTypes.forEach((eventType: MidiEventTypes) =>
+    input.addListener(eventType, "all", (event) => onMidiEvent(event))
+  );
 };
 
 // MIDI events
@@ -32,38 +74,30 @@ WebMidi.enable((err) => {
   // Retrieve an input by index
   const input = WebMidi.inputs[0];
 
-  input.addListener("noteon", "all", (event) => handleNoteOnEvent(event));
-  input.addListener("noteoff", "all", (event) => handleNoteOffEvent(event));
+  listenTo(["noteon", "noteoff"])(input);
 });
 
-function handleNoteOnEvent(event: InputEventNoteon) {
+function onMidiEvent(event: InputEventNoteon | InputEventNoteoff) {
   console.debug("event", event);
   const {
     note: { name, octave },
+    type,
   } = event;
   const note = (name + octave) as Keys;
   const square = document.getElementById(note);
-  lightUp(square);
-  playback(note);
-}
-
-function handleNoteOffEvent(event: InputEventNoteoff) {
-  console.debug("event", event);
-  const {
-    note: { name, octave },
-  } = event;
-  const note = (name + octave) as Keys;
-  const square = document.getElementById(note);
-  lightOff(square);
+  toggleColor(square);
+  if (type === "noteon") {
+    playback(note);
+  }
 }
 
 // Mouse events
 const onSquareClick = (element: HTMLElement) => {
   const { id } = element;
-  lightUp(element);
+  toggleColor(element);
   playback(id as Keys);
   setTimeout(() => {
-    lightOff(element);
+    toggleColor(element);
   }, 50);
 };
 
@@ -73,3 +107,11 @@ Array.from(squares).forEach((element: HTMLElement) =>
     element.addEventListener(event, () => onSquareClick(element))
   )
 );
+
+// function GetChar(event) {
+//   const chCode = "charCode" in event ? event.charCode : event.keyCode;
+//   console.log("The Unicode character code is: " + chCode);
+//   play(sampleBuffer);
+// }
+
+// document.onkeypress = (event) => GetChar(event);
