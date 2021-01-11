af1a2e86e09b9b2fef83390889793372f1d9382b
Stéphane B <stillfury@hotmail.com>
2021-01-10 19:35:35 -0500

    hookify logic in SamplesFetcher component

--- src/components/SamplesFetcher.tsx
+++ src/components/SamplesFetcher.tsx
@@ -1,49 +1,10 @@
-import JSZip from 'jszip';
-import React, { MouseEvent } from 'react';
-import makeSampler from '../models/sampler';
-import { makeSamplesTable } from '../models/samples-map';
-import { useSamplerContext } from '../services/sampler';
-import { getSampler, Sample, useSamplesContext } from '../services/samples';
-
-const LOCAL_URL = '/google-drive/';
-const PATH = 'uc?id=';
-const ID = '10ZKzj_ihTSxDvnk-Yt9QE61vaD-q4d-v';
-const request = new Request(LOCAL_URL + PATH + ID, { redirect: 'follow' });
+import React from 'react';
+import { useSamplesFetcher } from '../hooks';
 
 const SamplesFetcher = () => {
-  const { dispatch, setSamplesAreLoading } = useSamplesContext();
-  const { currentOctave } = useSamplerContext();
-  const handleOnClick = async (event: MouseEvent) => {
-    event.preventDefault();
-    setSamplesAreLoading(true);
-    try {
-      const response = await fetch(request);
-      const blob = await response.blob();
-      const { files } = await JSZip.loadAsync(blob);
-
-      const localSamples = await Promise.all(
-        Object.keys(files).map(
-          filename =>
-            new Promise<Sample>(async resolve => {
-              const fileData = await files[filename].async('arraybuffer');
-              resolve({
-                fileName: filename,
-                arrayBuffer: fileData,
-              });
-            })
-        )
-      );
-
-      const samplesTable = makeSamplesTable(localSamples, currentOctave);
-      const sampler = await makeSampler(samplesTable);
-      dispatch(getSampler(sampler));
-    } catch (e) {
-      console.error(e);
-    }
-    setSamplesAreLoading(false);
-  };
+  const { getRemoteSamples } = useSamplesFetcher();
   if (process.env.NODE_ENV === 'development') {
-    return <button onClick={handleOnClick}>Test</button>;
+    return <button onClick={getRemoteSamples}>Get remote samples</button>;
   }
   return null;
 };
--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import JSZip from 'jszip';
 import { SAMPLES_URL, SAMPLE_NAMES } from './constants';
 import makeSampler from './models/sampler';
 import { makeSamplesTable } from './models/samples-map';
@@ -51,3 +52,42 @@ const fetchSamples = async () =>
       };
     })
   );
+
+const LOCAL_URL = '/google-drive/';
+const PATH = 'uc?id=';
+const ID = '10ZKzj_ihTSxDvnk-Yt9QE61vaD-q4d-v';
+const request = new Request(LOCAL_URL + PATH + ID, { redirect: 'follow' });
+
+export const useSamplesFetcher = () => {
+  const { dispatch, setSamplesAreLoading } = useSamplesContext();
+  const { currentOctave } = useSamplerContext();
+  const getRemoteSamples = async () => {
+    setSamplesAreLoading(true);
+    try {
+      const response = await fetch(request);
+      const blob = await response.blob();
+      const { files } = await JSZip.loadAsync(blob);
+
+      const remoteSamples = await Promise.all(
+        Object.keys(files).map(
+          filename =>
+            new Promise<Sample>(async resolve => {
+              const fileData = await files[filename].async('arraybuffer');
+              resolve({
+                fileName: filename,
+                arrayBuffer: fileData,
+              });
+            })
+        )
+      );
+
+      const samplesTable = makeSamplesTable(remoteSamples, currentOctave);
+      const sampler = await makeSampler(samplesTable);
+      dispatch(getSampler(sampler));
+    } catch (e) {
+      console.error(e);
+    }
+    setSamplesAreLoading(false);
+  };
+  return { getRemoteSamples };
+};
