1c5d23c16213e15b507323a99d63e57d9b0b6ba2
Stéphane B <stillfury@hotmail.com>
2021-05-12 11:10:23 -0400

    add images to codebase

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,11 +4,15 @@ import Sampler from '../Sampler';
 import MidiConnector from '../MidiConnector';
 import { FileLoader } from '../FileLoader';
 import './style.sass';
+import logo from 'url:../../images/logo.svg';
 
 const App = () => (
   <Providers>
     <>
       <div className="container">
+        <header>
+          <img src={`${logo}`} />
+        </header>
         <div className="buttons">
           <FileLoader />
         </div>
